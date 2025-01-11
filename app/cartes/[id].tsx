af4f78f79c974985ff94ad5a0caaf38a5c6a3bde
mxou <maximilien.dlrm@gmail.com>
2025-01-11 16:57:51 +0100

    ajout dans la page du component loading

--- app/cartes/[id].tsx
+++ app/cartes/[id].tsx
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { useLocalSearchParams } from 'expo-router';
-import { View, Text, ActivityIndicator } from 'react-native';
-import { fetchData } from '../../src/api';
+import React, { useEffect, useState } from "react";
+import { useLocalSearchParams } from "expo-router";
+import { View, Text, ActivityIndicator } from "react-native";
+import { fetchData } from "../../src/api";
 import "react-native-reanimated";
 
-import Header from '../../src/components/Header';
-import CardSlider from '../../src/components/CardSlider';
-import styles from './../../assets/styles/carte_style.js';
-import HomeButton from '../../src/components/HomeButton';
-import ErrorDisplay from '../../src/components/ErrorDisplay';
+import Header from "../../src/components/Header";
+import CardSlider from "../../src/components/CardSlider";
+import styles from "./../../assets/styles/carte_style.js";
+import HomeButton from "../../src/components/HomeButton";
+import ErrorDisplay from "../../src/components/ErrorDisplay";
+import LoadingDisplay from "../../src/components/LoadingDisplay";
 
 type Card = {
   id_carte: number;
@@ -21,7 +22,7 @@ export default function CartesScreen() {
   const { id } = useLocalSearchParams(); // Récupère l'ID du deck depuis l'URL
   console.log(id);
   const [cards, setCards] = useState<Card[]>([]);
-  const [deckTitle, setDeckTitle] = useState<string>(''); // Ajout du titre
+  const [deckTitle, setDeckTitle] = useState<string>(""); // Ajout du titre
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
 
@@ -31,13 +32,13 @@ export default function CartesScreen() {
       setLoading(false);
       return;
     }
-    
+
     fetchData(`https://srochedix.alwaysdata.net/ReignApi/api/v1/cartes/deck/${id}`)
       .then((result) => {
-        console.log('Réponse API :', result);
-        if (result.status === 'success' && result.deck && Array.isArray(result.deck.cartes)) {
+        console.log("Réponse API :", result);
+        if (result.status === "success" && result.deck && Array.isArray(result.deck.cartes)) {
           setCards(result.deck.cartes);
-          setDeckTitle(result.deck.titre_deck || 'Deck Inconnu'); //Récupération du titre
+          setDeckTitle(result.deck.titre_deck || "Deck Inconnu"); //Récupération du titre
         } else {
           setError(new Error("Les données renvoyées par l'API ne sont pas valides."));
         }
@@ -46,19 +47,15 @@ export default function CartesScreen() {
       .finally(() => setLoading(false));
   }, [id]);
 
-  if (loading) return <ActivityIndicator size="large" color="#fc035e" />;
-  // if (error) return <Text style={styles.errorText}>Erroree: {error.message}</Text>;
- if (error) return <ErrorDisplay error={error} />;
+  // if (loading) return <ActivityIndicator size="large" color="#fc035e" />;
+  if (loading) return <LoadingDisplay />;
+  if (error) return <ErrorDisplay error={error} />;
 
   return (
     <View style={styles.container}>
       <Header />
       <Text style={styles.title}>{deckTitle}</Text>
-      {cards.length > 0 ? (
-        <CardSlider cards={cards} />
-      ) : (
-        <Text style={styles.noCardsText}>Aucune carte disponible pour ce deck.</Text>
-      )}
+      {cards.length > 0 ? <CardSlider cards={cards} /> : <Text style={styles.noCardsText}>Aucune carte disponible pour ce deck.</Text>}
     </View>
   );
 }
